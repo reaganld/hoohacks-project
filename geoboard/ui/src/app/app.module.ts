50efff6c68f00eb9708353cbd8b4e0a6d7959afb
klrao <klrao@ncsu.edu>
2023-03-25 19:46:47 -0400

    minor code changes

--- geoboard/ui/src/app/app.module.ts
+++ geoboard/ui/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
 import { ViewComponent } from './view/view.component';
 import { EditComponent } from './edit/edit.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent,
     ViewComponent,
-    EditComponent
+    EditComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
