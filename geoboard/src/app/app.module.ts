1ccc69f045dc6af068ae50025563380ca0db1ff0
klrao <klrao@ncsu.edu>
2023-03-25 14:34:12 -0400

    boilerplate code for pages

--- geoboard/src/app/app.module.ts
+++ geoboard/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { ViewComponent } from './view/view.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ViewComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
