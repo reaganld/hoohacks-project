f043821f1180ecf4860f4fa8f6233b95dcd74140
klrao <klrao@ncsu.edu>
2023-03-25 14:50:06 -0400

    routing done

--- geoboard/src/app/app-routing.module.ts
+++ geoboard/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditComponent } from './edit/edit.component';
+import { LandingComponent } from './landing/landing.component';
+import { ViewComponent } from './view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'landing', component: LandingComponent },
+    { path: 'view', component: ViewComponent },
+    { path: 'edit', component: EditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
