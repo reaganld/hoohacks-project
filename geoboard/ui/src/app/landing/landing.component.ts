bbc7eda4df1e2b39ab8a6bd19083d60476a846c5
klrao <klrao@ncsu.edu>
2023-03-26 09:19:48 -0400

    broken code push

--- geoboard/ui/src/app/landing/landing.component.ts
+++ geoboard/ui/src/app/landing/landing.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import mergeImages from 'merge-images';
 import { ImageService } from '../image/image.service';
-import { Image } from '../image/image';
 import { Injectable } from '@angular/core';
 import * as $ from 'jquery'
 
