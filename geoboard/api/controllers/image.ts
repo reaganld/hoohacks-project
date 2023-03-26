000effd6e2f28c0a5985fdf1d7fbe17fcbd6782f
klrao <klrao@ncsu.edu>
2023-03-26 11:15:03 -0400

    attempted backend

--- geoboard/api/controllers/image.ts
+++ geoboard/api/controllers/image.ts
@@ -9,6 +9,22 @@ const getImages = async (req, res) => {
 const postImage = async (req, res) => {
   const { coords} = req.body;
   const imagePath = 'http://localhost:3000/images/' + req.file.filename; // Note: set path dynamically
+  //   const imagePath = 'http://localhost:3000/images/' + req.file.filename; // Note: set path dynamically
+//   const matchingImage = await Image.findOne({ coords: coords });
+//   const mFilepath = matchingImage.imagePath;
+//   mergeImages([imagePath, mFilepath])
+//   .then( (b64) => {
+//       let str:string = b64;
+//       const imageName = 'test111';
+//       const byteString = window.atob(str.split(",")[1]);
+//       const arrayBuffer = new ArrayBuffer(byteString.length);
+//       const int8Array = new Uint8Array(arrayBuffer);
+//       for (let i = 0; i < byteString.length; i++) {
+//         int8Array[i] = byteString.charCodeAt(i);
+//       }
+//       const blob = new Blob([int8Array], { type: 'image/png' });
+//       const imageFile = new File([blob], imageName, { type: 'image/png' });
+//     });
   const image = new Image({
     coords,
     imagePath,
